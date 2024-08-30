804898468161b57a553d11a71d70e17537e480dc
Rodamant <153533506+Rodamant@users.noreply.github.com>
2024-08-30 15:41:21 +0300

    add PushFront, PopBack, Clear and test

--- Test_tast/Test_tast/vector.cpp
+++ Test_tast/Test_tast/vector.cpp
@@ -19,6 +19,33 @@ void Vector::PushBack(int value)
 
 }
 
+void Vector::PushFront(int value)
+{
+	int* newVector = new int[size + 1];
+
+	for (int i = 1; i < size; i++)
+	{
+		newVector[i] = vector[i];
+	}
+	newVector[0] = value;
+	delete[] vector;
+	vector = newVector;
+	size++;
+}
+
+void Vector::PopBack()
+{
+	size--;
+	int* newVector = new int[size];
+
+	for (int i = 0; i < size; i++)
+	{
+		newVector[i] = vector[i];
+	}
+	delete[] vector;
+	vector = newVector;
+}
+
 int Vector::Size() const
 {
 	return size;
@@ -32,6 +59,14 @@ const int& Vector::operator[](int index) const
 int& Vector::operator[](int index)
 {
 	return vector[index];
+}
+
+void Vector::Clear()
+{
+	size = 0;
+	int* newVector = new int[size];
+	delete[] vector;
+	vector = newVector;
 }
 
 
--- Test_tast/Test_tast/vector.h
+++ Test_tast/Test_tast/vector.h
@@ -7,9 +7,12 @@ public:
 	Vector();
 	~Vector();
 	void PushBack(int value);
+	void PushFront(int value);
+	void PopBack();
 	int Size() const;
 	int& operator [](int index); 
 	const int& operator [](int index) const; 
+	void Clear();
 private:
 	int size = 0;
 	int* vector = nullptr;
