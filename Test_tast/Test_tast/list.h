8e6757b49a9a9ec05f20d050ac116fc8d75a6734
Rodamant <153533506+Rodamant@users.noreply.github.com>
2024-09-11 15:51:31 +0300

    add List base

--- Test_tast/Test_tast/list.h
+++ Test_tast/Test_tast/list.h
@@ -1,1 +1,40 @@
 #pragma once
+template<typename T>
+class List
+{
+public:
+	List();
+	~List();
+
+private:
+	template<typename T>
+	class Node
+	{
+	public:
+		Node* pNext;
+		T data;
+		Node(T data = T(), Node* pNext = nullptr)
+		{
+			this->data = data;
+			this->pNext = pNext;
+		}
+	};
+
+
+	Node<T> *head;
+	int size;
+
+};
+
+
+template<typename T>
+inline List<T>::List()
+{
+	size = 0;
+	head = nullptr;
+}
+
+template<typename T>
+inline List<T>::~List()
+{
+}
